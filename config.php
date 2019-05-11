7925e0051cb94fb97df88059ff13afaf67384cf4
ejestep <50462693+ejestep@users.noreply.github.com>
2019-05-11 01:52:40 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+return [
+    //
+    'consumer_key'      => 'yf9Pd0qG4EiNhMv8gjWpFXcdV',
+    'consumer_secret'   => 'OEB3moR9U3jJZfue7daLe8ggGEFtkCA4fAbxZnPRmgOZtAlU94',
+ 
+    //
+    'url_login'         => 'http://localhost/twitter_login.php',
+    'url_callback'      => 'http://localhost/twitter_callback.php',
+];
